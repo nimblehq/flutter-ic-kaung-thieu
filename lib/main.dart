21b507e9e319ddae7bdaaba1b1ed284312123312
Doan Thieu <thieurom@gmail.com>
2023-04-11 09:32:48 +0700

    [#6] Create Login screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@ import 'package:flutter_config/flutter_config.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:survey_flutter/screens/login/login_screen.dart';
+import 'package:survey_flutter/theme/app_theme.dart';
+import 'package:survey_flutter/screens/login/login.dart';
 import 'package:survey_flutter/screens/splash/splash.dart';
 import 'package:survey_flutter/theme/app_theme.dart';
 
