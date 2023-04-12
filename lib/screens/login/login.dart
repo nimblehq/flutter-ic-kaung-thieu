cf1f5aade982f53e4498ad58ab35f8f10d29d9c7
Doan Thieu <thieurom@gmail.com>
2023-04-12 21:45:43 +0700

    [#6] Update localized strings for login form

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -2,9 +2,9 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:survey_flutter/gen/assets.gen.dart';
 import 'package:survey_flutter/utils/keyboard_manager.dart';
-import 'package:survey_flutter/screens/login/login_component_id.dart';
 import 'package:survey_flutter/screens/widgets/form_field_decoration.dart';
 import 'package:survey_flutter/screens/widgets/blur_image.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 const routePathLoginScreen = '/login';
 
@@ -147,36 +147,33 @@ class _LoginForm extends StatefulWidget {
 }
 
 class _LoginFormState extends State<_LoginForm> {
-  TextFormField _emailTextField(TextStyle? textStyle) {
-    return TextFormField(
-      key: LoginScreenKey.emailTextField,
-      keyboardType: TextInputType.emailAddress,
-      autocorrect: false,
-      decoration: FormFieldDecoration(
-        hint: 'Email',
-        hintTextStyle: textStyle,
-      ),
-      style: textStyle,
-    );
-  }
+  AppLocalizations get _localizations => AppLocalizations.of(context)!;
+  TextTheme get _textTheme => Theme.of(context).textTheme;
 
-  TextFormField _passwordTextField(TextStyle? textStyle) {
-    return TextFormField(
-      key: LoginScreenKey.passwordTextField,
-      keyboardType: TextInputType.emailAddress,
-      obscureText: true,
-      obscuringCharacter: "●",
-      decoration:
-          FormFieldDecoration(hint: 'Password', hintTextStyle: textStyle),
-      style: textStyle,
-    );
-  }
+  TextFormField get _emailTextField => TextFormField(
+        keyboardType: TextInputType.emailAddress,
+        autocorrect: false,
+        decoration: FormFieldDecoration(
+          hint: _localizations.emailInputHint,
+          hintTextStyle: _textTheme.bodyLarge,
+        ),
+        style: _textTheme.bodyLarge,
+      );
 
-  ElevatedButton _loginButton(TextStyle? textStyle) {
-    return ElevatedButton(
-      key: LoginScreenKey.loginButton,
+  TextFormField get _passwordTextField => TextFormField(
+        keyboardType: TextInputType.emailAddress,
+        obscureText: true,
+        obscuringCharacter: "●",
+        decoration: FormFieldDecoration(
+            hint: _localizations.passwordInputHint,
+            hintTextStyle: _textTheme.bodyLarge),
+        style: _textTheme.bodyLarge,
+      );
+
+  ElevatedButton get _loginButton => ElevatedButton(
       style: ButtonStyle(
-        textStyle: MaterialStateProperty.all(textStyle),
+        textStyle:
+            MaterialStateProperty.all(Theme.of(context).textTheme.labelLarge),
         backgroundColor: MaterialStateProperty.all(Colors.white),
         foregroundColor: MaterialStateProperty.all(Colors.black),
         overlayColor: MaterialStateProperty.all(Colors.black12),
@@ -186,13 +183,12 @@ class _LoginFormState extends State<_LoginForm> {
           ),
         ),
       ),
-      child: Text('Log in'),
+      child: Text(_localizations.loginButton),
       onPressed: () {
         KeyboardManager.dismiss(context);
         // TODO - implement login
       },
     );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -200,18 +196,18 @@ class _LoginFormState extends State<_LoginForm> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          _emailTextField(Theme.of(context).textTheme.bodyLarge),
+          _emailTextField,
           const SizedBox(
             height: 20,
           ),
-          _passwordTextField(Theme.of(context).textTheme.bodyLarge),
+          _passwordTextField,
           const SizedBox(
             height: 20,
           ),
           SizedBox(
             height: 56,
             width: double.infinity,
-            child: _loginButton(Theme.of(context).textTheme.labelLarge),
+            child: _loginButton,
           )
         ],
       ),
--- lib/screens/login/login_component_id.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class LoginScreenKey {
-  LoginScreenKey._();
-
-  static const emailTextField = Key('emailTextField');
-  static const passwordTextField = Key('passwordTextField');
-  static const loginButton = Key('loginButton');
-}
