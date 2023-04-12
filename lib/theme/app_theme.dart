0df166421b2f91063eacd872f8bd4cf1b9835860
Doan Thieu <thieurom@gmail.com>
2023-04-12 22:16:22 +0700

    [#6] Update text fields' cursor color

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -3,12 +3,13 @@ import 'package:survey_flutter/gen/fonts.gen.dart';
 
 class AppTheme {
   static ThemeData get light => ThemeData(
-        fontFamily: FontFamily.neuzeit,
-        textTheme: const TextTheme(
-          bodyLarge: TextStyle(
-              color: Colors.white, fontSize: 17, fontWeight: FontWeight.w400),
-          labelLarge: TextStyle(
-              color: Colors.red, fontSize: 17, fontWeight: FontWeight.w800),
-        ),
-      );
+      fontFamily: FontFamily.neuzeit,
+      textTheme: const TextTheme(
+        bodyLarge: TextStyle(
+            color: Colors.white, fontSize: 17, fontWeight: FontWeight.w400),
+        labelLarge: TextStyle(
+            color: Colors.red, fontSize: 17, fontWeight: FontWeight.w800),
+      ),
+      textSelectionTheme:
+          const TextSelectionThemeData(cursorColor: Colors.white));
 }
