7f62fa532f0067228ea1d72e1934ec9dbba11b62
Kaung Khant Soe <kaungkhantsoe.2712@gmail.com>
2023-05-08 18:58:08 +0700

    [#61] Fix wrong login response type

--- lib/usecases/login_use_case.dart
+++ lib/usecases/login_use_case.dart
@@ -1,20 +1,20 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:survey_flutter/api/repository/login_repository.dart';
 import 'package:survey_flutter/model/login_parameters.dart';
-import 'package:survey_flutter/model/response/login_response.dart';
+import 'package:survey_flutter/model/response/login_data_response.dart';
 import 'package:survey_flutter/usecases/base/base_use_case.dart';
 
 final loginUseCaseProvider = Provider<LoginUseCase>((ref) {
   return LoginUseCase(ref.watch(loginRepositoryProvider));
 });
 
-class LoginUseCase extends UseCase<LoginResponse, LoginParameters> {
+class LoginUseCase extends UseCase<LoginDataResponse, LoginParameters> {
   final LoginRepository _loginRepository;
 
   LoginUseCase(this._loginRepository);
 
   @override
-  Future<Result<LoginResponse>> call(LoginParameters params) async {
+  Future<Result<LoginDataResponse>> call(LoginParameters params) async {
     try {
       final result = await _loginRepository.login(
         email: params.email,
