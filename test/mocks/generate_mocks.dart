e7bed6d24b4d138a3cdd692777b4f4d04a14e5dc
Kaung Khant Soe <kaungkhantsoe.2712@gmail.com>
2023-05-08 18:17:44 +0700

    [#12] Fix rebase issue

--- test/mocks/generate_mocks.dart
+++ test/mocks/generate_mocks.dart
@@ -4,7 +4,6 @@ import 'package:survey_flutter/api/api_service.dart';
 import 'package:mockito/annotations.dart';
 import 'package:survey_flutter/api/repository/auth_repository.dart';
 import 'package:survey_flutter/api/repository/login_repository.dart';
-import 'package:survey_flutter/api/repository/shared_preference_repository.dart';
 import 'package:survey_flutter/api/storage/shared_preference.dart';
 
 @GenerateMocks([
@@ -14,7 +13,6 @@ import 'package:survey_flutter/api/storage/shared_preference.dart';
   FlutterSecureStorage,
   AuthRepository,
   SharedPreference,
-  SharedPreferenceRepository,
 ])
 main() {
   // empty class to generate mock repository classes
