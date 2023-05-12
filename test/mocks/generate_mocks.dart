f40c5e0fa217bf0491cc793fd8f2069644d2689e
Kaung Khant Soe <kaungkhantsoe.2712@gmail.com>
2023-05-12 10:23:36 +0700

    [#10] Change LoginRepo to AuthRepo

--- test/mocks/generate_mocks.dart
+++ test/mocks/generate_mocks.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:survey_flutter/api/api_service.dart';
 import 'package:mockito/annotations.dart';
-import 'package:survey_flutter/api/repository/login_repository.dart';
+import 'package:survey_flutter/api/repository/auth_repository.dart';
 import 'package:survey_flutter/api/storage/shared_preference.dart';
 import 'package:survey_flutter/usecases/login_use_case.dart';
 
@@ -11,7 +11,7 @@ import '../util/async_listener.dart';
 @GenerateMocks([
   ApiService,
   DioError,
-  LoginRepository,
+  AuthRepository,
   FlutterSecureStorage,
   SharedPreference,
   LoginUseCase,
