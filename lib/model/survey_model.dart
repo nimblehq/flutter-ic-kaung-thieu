80f4b42922ffb919f565dc839f1f3159b72d2876
Kaung Khant Soe <kaungkhantsoe.2712@gmail.com>
2023-05-16 04:21:55 +0700

    [#31] Integrate survey list

--- lib/model/survey_model.dart
+++ lib/model/survey_model.dart
@@ -1,3 +1,5 @@
+import 'package:survey_flutter/model/hives/survey.dart';
+
 class SurveyModel {
   final String title;
   final String description;
@@ -8,4 +10,14 @@ class SurveyModel {
     required this.description,
     required this.coverImageUrl,
   });
+}
+
+extension SurveyModelMapping on Survey {
+  SurveyModel toSurveyModel() {
+    return SurveyModel(
+      title: title,
+      description: description,
+      coverImageUrl: '${coverImageUrl}l',
+    );
+  }
 }
--- lib/screens/home/home_view_model.dart
+++ lib/screens/home/home_view_model.dart
@@ -2,33 +2,48 @@ import 'dart:async';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
+import 'package:survey_flutter/model/hives/survey.dart';
 import 'package:survey_flutter/model/survey_model.dart';
 import 'package:survey_flutter/model/profile_model.dart';
+import 'package:survey_flutter/model/surveys_parameters.dart';
 import 'package:survey_flutter/usecases/base/base_use_case.dart';
+import 'package:survey_flutter/usecases/get_cached_number_of_page_use_case.dart';
+import 'package:survey_flutter/usecases/get_cached_surveys_use_case.dart';
+import 'package:survey_flutter/usecases/get_surveys_use_case.dart';
+
+const pageSize = 5;
 
 final homeViewModelProvider =
     AsyncNotifierProvider.autoDispose<HomeViewModel, void>(HomeViewModel.new);
 
 class HomeViewModel extends AutoDisposeAsyncNotifier<void> {
-  final _surveyCache = <SurveyModel>[];
+  var _surveyCache = <SurveyModel>[];
 
   final _surveys = StreamController<List<SurveyModel>>();
+
   Stream<List<SurveyModel>> get surveys => _surveys.stream;
 
   final _profile = StreamController<ProfileModel>();
+
   Stream<ProfileModel> get profile => _profile.stream;
 
   final _currentDate = StreamController<String>();
+
   Stream<String> get currentDate => _currentDate.stream;
 
   final _isError = StreamController<String>();
+
   Stream<String> get isError => _isError.stream;
 
   final _isLoadMore = StreamController<bool>();
+
   Stream<bool> get isLoadMore => _isLoadMore.stream;
 
   final _shouldShowShimmer = StreamController<bool>();
+
   Stream<bool> get shouldShowShimmer => _shouldShowShimmer.stream;
+
+  var _pageNumber = 0;
 
   @override
   FutureOr<void> build() {
@@ -69,26 +84,67 @@ class HomeViewModel extends AutoDisposeAsyncNotifier<void> {
   }
 
   Future<void> getSurveyList() async {
+    final numberOfPage =
+        await ref.read(getCachedNumberOfPageUseCaseProvider).call();
+    if (numberOfPage is Success<int> &&
+        numberOfPage.value == _pageNumber &&
+        _pageNumber != 0) {
+      return;
+    }
+
+    _pageNumber += 1;
+
     if (_surveyCache.isEmpty) {
       showOrHideShimmer(true);
     } else {
       showOrHideLoadMore(true);
     }
 
-    // TODO replace with usecase in integration
-    Future.delayed(const Duration(seconds: 2), () {
-      _surveyCache.addAll((_mockList(_surveyCache.length) as Success).value);
-      _surveys.add(_surveyCache);
+    _getCacheSurveys();
+  }
+
+  void _getCacheSurveys() async {
+    final result = await ref.read(getCachedSurveyUseCaseProvider).call(
+          SurveysParameters(
+            pageNumber: _pageNumber,
+            pageSize: pageSize,
+          ),
+        );
+
+    if (result is Success) {
+      final cachedSurveys = (result as Success<List<Survey>>).value;
+      final previousSize = (_pageNumber * pageSize) - pageSize;
+      if (cachedSurveys.length > previousSize) {
+        _surveyCache =
+            cachedSurveys.map((survey) => survey.toSurveyModel()).toList();
+        _surveys.add(_surveyCache);
+        showOrHideShimmer(false);
+        showOrHideLoadMore(false);
+      } else {
+        _getSurveysFromNetwork();
+      }
+    } else if (result is Failed) {
+      _isError.add((result as Failed).getErrorMessage());
       showOrHideShimmer(false);
       showOrHideLoadMore(false);
-    });
+    }
+  }
 
-    // TODO remove this in integration
-    Future.delayed(const Duration(seconds: 4), () {
-      var exception = Exception('Test error');
-      _surveys.addError(exception, StackTrace.current);
-      _isError.add('Test error');
-    });
+  void _getSurveysFromNetwork() async {
+    final result = await ref.read(getSurveysUseCaseProvider).call(
+          SurveysParameters(
+            pageNumber: _pageNumber,
+            pageSize: pageSize,
+          ),
+        );
+
+    if (result is Success) {
+      _getCacheSurveys();
+    } else if (result is Failed) {
+      _isError.add((result).getErrorMessage());
+      showOrHideShimmer(false);
+      showOrHideLoadMore(false);
+    }
   }
 
   Future<void> getProfile() async {
@@ -96,40 +152,5 @@ class HomeViewModel extends AutoDisposeAsyncNotifier<void> {
     Future.delayed(const Duration(seconds: 5), () {
       _profile.add(ProfileModel(imageUrl: 'https://picsum.photos/id/64/100'));
     });
-  }
-
-  Result<List<SurveyModel>> _mockList(int size) {
-    return Success([
-      SurveyModel(
-        title: '${size + 1} Working from home Check-In',
-        description:
-            'We would like to know how you feel about our work from home. We would like to know how you feel about our work from home.',
-        coverImageUrl: 'https://picsum.photos/376/812',
-      ),
-      SurveyModel(
-        title: '${size + 2} Working from home Check-In',
-        description:
-            'We would like to know how you feel about our work from home. We would like to know how you feel about our work from home.',
-        coverImageUrl: 'https://picsum.photos/376/812',
-      ),
-      SurveyModel(
-        title: '${size + 3} Working from home Check-In',
-        description:
-            'We would like to know how you feel about our work from home. We would like to know how you feel about our work from home.',
-        coverImageUrl: 'https://picsum.photos/376/812',
-      ),
-      SurveyModel(
-        title: '${size + 4} Working from home Check-In',
-        description:
-            'We would like to know how you feel about our work from home. We would like to know how you feel about our work from home.',
-        coverImageUrl: 'https://picsum.photos/376/812',
-      ),
-      SurveyModel(
-        title: '${size + 5} Working from home Check-In',
-        description:
-            'We would like to know how you feel about our work from home. We would like to know how you feel about our work from home.',
-        coverImageUrl: 'https://picsum.photos/376/812',
-      ),
-    ]);
   }
 }
