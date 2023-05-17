7d246976817229bbe967ed54c8191708854ea890
Kaung Khant Soe <kaungkhantsoe.2712@gmail.com>
2023-05-17 07:59:00 +0700

    [#31] Add test case for HomeViewModel

--- test/mocks/generate_mocks.dart
+++ test/mocks/generate_mocks.dart
@@ -6,6 +6,9 @@ import 'package:survey_flutter/api/repository/auth_repository.dart';
 import 'package:survey_flutter/api/repository/survey_repository.dart';
 import 'package:survey_flutter/api/storage/hive_storage.dart';
 import 'package:survey_flutter/api/storage/shared_preference.dart';
+import 'package:survey_flutter/model/surveys_parameters.dart';
+import 'package:survey_flutter/usecases/get_cached_surveys_use_case.dart';
+import 'package:survey_flutter/usecases/get_surveys_use_case.dart';
 import 'package:survey_flutter/usecases/login_use_case.dart';
 
 import '../util/async_listener.dart';
@@ -20,6 +23,9 @@ import '../util/async_listener.dart';
   AsyncListener,
   SurveyRepository,
   HiveStorage,
+  GetSurveysUseCase,
+  GetCachedSurveysUseCase,
+  SurveysParameters,
 ])
 main() {
   // empty class to generate mock repository classes
